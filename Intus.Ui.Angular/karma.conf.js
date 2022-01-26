4d987201f9738c35c7b42e9419ce556a88e12fb7
Axel Breekweg <axel.breekweg@allphi.eu>
2022-01-26 19:04:49 +0100

    Restarted init

--- Intus.Ui.Angular/karma.conf.js
+++ Intus.Ui.Angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Intus-Ui-Angular'),
+      dir: require('path').join(__dirname, './coverage/Intus-Angular-Ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Intus.Ui.Angular/src/app/app.component.ts
+++ Intus.Ui.Angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Intus-Ui-Angular';
+  title = 'Intus-Angular-Ui';
 }
